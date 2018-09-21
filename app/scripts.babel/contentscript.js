642e0ec3aa73302a493905fea783f71b3e803f17
加藤北斗 <hokuto15@gmail.com>
2018-09-21 21:06:28 +0900

    トリガーをダブルクリックに変更

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,9 +1,13 @@
 $(() => {
-	$('body').on('click', (obj) => {
+	$('body').find('a').on('click',(e) => {
+		e.preventDefault();
+	});
+	
+	$('body').on('dblclick',(obj) => {
 		let target = $(obj.target).text();
-		let targetMatch = target.match(/^((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g);
-		if(targetMatch) {
-			window.open(target,'newtab');
+		let targetMatch = target.match(/((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g);
+		let targetMatchAnchor = target.match(/^<a href/g);
+		console.log(target,targetMatch,targetMatchAnchor);
 		}
 	});
 });
